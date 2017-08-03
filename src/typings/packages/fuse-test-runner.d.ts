eea35ba8e98739f72ec0fc05cf2552b5d91e6364
Tomas Trescak <tomi.trescak@gmail.com>
2017-08-03 13:33:22 +1000

    All is covered

--- src/typings/packages/fuse-test-runner.d.ts
+++ src/typings/packages/fuse-test-runner.d.ts
@@ -1,6 +1,7 @@
 declare function xit(name: string, implementation: () => void); 
 declare function it(name: string, implementation: () => void); 
 declare function describe(name: string, implementation: () => void); 
+declare function xdescribe(name: string, implementation: () => void); 
 declare function before(implementation: () => void); 
 declare function beforeAll(implementation: () => void); 
 declare function beforeEach(implementation: () => void); 
